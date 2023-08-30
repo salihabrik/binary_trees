dae77c74b1493cabba472f8aac60dc08e72c8c8e
salihabrik <briksaliha@gmail.com>
2023-08-30 09:02:05 +0200

    Resolved conflicts in 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,40 +1,43 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree to measure the height
- *
- * Return: Height of the tree, 0 if tree is NULL
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node
+ * Return: 0 if tree is NULL
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+
+int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t leftheight, rightheight;
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	leftheight = binary_tree_height(tree->left);
+	rightheight = binary_tree_height(tree->right);
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+	return (leftheight - rightheight);
 }
 
 /**
- * binary_tree_balance - Measures the balance factor of a binary tree
- * @tree: Pointer to the root node of the tree to measure the balance factor
- *
- * Return: Balance factor of the tree, 0 if tree is NULL
+ * binary_tree_height - calculate height of a binary tree
+ * @tree: pointer to the root node
+ * Return: 0 on failure || height of the tree
  */
-int binary_tree_balance(const binary_tree_t *tree)
+
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left_height, right_height;
-if (tree == NULL)
-	return (0);
+	size_t leftheight = 0;
+	size_t rightheight = 0;
 
-	left_height = (int)binary_tree_height(tree->left);
-	right_height = (int)binary_tree_height(tree->right);
+	if (tree == NULL)
+		return (0);
 
-	return (left_height - right_height);
+	leftheight += 1 + binary_tree_height(tree->left);
+	rightheight += 1 + binary_tree_height(tree->right);
+
+	if (leftheight > rightheight)
+		return (leftheight);
+	else
+		return (rightheight);
 }
-
-	if (tree == NULL
