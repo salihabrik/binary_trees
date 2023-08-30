28423765657226ee2223ea8762beebeced226f26
salihabrik <briksaliha@gmail.com>
2023-08-30 09:01:20 +0200

    Resolved conflicts in 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -28,9 +28,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left_height, right_height;
-    
-	if (tree == NULL)
-		return (0);
+if (tree == NULL)
+	return (0);
 
 	left_height = (int)binary_tree_height(tree->left);
 	right_height = (int)binary_tree_height(tree->right);
