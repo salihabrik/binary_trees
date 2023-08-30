f4abb577f7c25fe00cfaa8e2c1e6637a971f9468
salihabrik <briksaliha@gmail.com>
2023-08-30 08:17:50 +0200

    goes through a binary tree using in-order traversalgit push

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -12,7 +12,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
-	binary_tree_inorder(tree->left, func);   /* Traverse the left subtree */
-	func(tree->n);                           /* Call the function on the current node */
-	binary_tree_inorder(tree->right, func);  /* Traverse the right subtree */
+binary_tree_inorder(tree->left, func);
+func(tree->n);
+binary_tree_inorder(tree->right, func);
 }
