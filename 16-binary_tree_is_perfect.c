317bc5752e3949ef77268661fab7d387e4182baa
salihabrik <briksaliha@gmail.com>
2023-08-30 23:46:56 +0200

    UPDATE

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,28 +1,64 @@
 #include "binary_trees.h"
 
+
 /**
- * binary_tree_is_perfect - Checks  ifis perfect
- * @tree: Pointer to the root node of the tree to check
+ * binary_tree_height - measures the height of a binary tree.
+ *@tree: pointer to the root node of the tree to measure the height.
+ * Return: if tree is NULL, your function must return 0.
  *
- * Return: 1 if the tree is perfect, 0 otherwise
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		int left = 0, right = 0;
+
+		if (tree->right)
+			right = 1 + binary_tree_height(tree->right);
+		if (tree->left)
+			left = 1 + binary_tree_height(tree->left);
+		if (left > right)
+			return (left);
+		else
+			return (right);
+	}
+	else
+		return (0);
+}
+/**
+ * binary_tree_size - measure the size of a binary tree.
+ *@tree: pointer to the root node of the tree to measure the size.
+ * Return: if tree is NULL, must return 0.
+ *
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	{
+		size_t n_left, n_right;
+
+		n_left = binary_tree_size(tree->left);
+		n_right = binary_tree_size(tree->right);
+		return (1 + n_left + n_right);
+	}
+}
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect.
+ *@tree: pointer to the root node of the tree to check.
+ * Return: if tree is NULL,return 0
+ *
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	size_t h = 0, perfect_size = 0;
 
 	if (tree == NULL)
 		return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	if (left_height == right_height)
-	{
-		if (tree->left == NULL && tree->right == NULL)
-			return (1);
-
+	h = binary_tree_height(tree);
+	perfect_size = (1 << (h + 1)) - 1;
+	if (perfect_size == binary_tree_size(tree))
+		return (1);
+	else
 		return (0);
-	}
-
-	return (0);
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,64 +1,25 @@
 #include "binary_trees.h"
 
-
 /**
- * binary_tree_is_perfect - Checks if a binary tree is perfect.
- * @tree: A pointer to the root node of the tree.
- *
- * Return: 1 if the tree is perfect, 0 otherwise.
- */
-int binary_tree_is_perfect(const binary_tree_t *tree)
-{
-    if (!tree)
-        return (0);
-
-    if (binary_tree_balance(tree) != 0)
-        return (0);
-
-    if (!tree->left && !tree->right)
-        return (1);
-
-    if (!tree->left || !tree->right)
-        return (0);
-
-    return (binary_tree_is_perfect(tree->left) &&
-            binary_tree_is_perfect(tree->right));
-}
-
-/**
- * binary_tree_balance - Measures the balance factor of a binary tree.
- * @tree: A pointer to the root node of the tree.
- *
- * Return: The balance factor.
- */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-    int left_height, right_height;
-
-    if (!tree)
-        return (0);
-
-    left_height = (int)binary_tree_height(tree->left);
-    right_height = (int)binary_tree_height(tree->right);
-
-    return (left_height - right_height);
-}
-
-/**
- * binary_tree_height - Measures the height of a binary tree.
- * @tree: A pointer to the root node of the tree.
- *
- * Return: The height of the tree.
+ * binary_tree_height - measures the height of a binary tree.
+ *@tree: pointer to the root node of the tree to measure the height.
+ * Return: if tree is NULL, your function must return 0.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+	if (tree)
+	{
+		int left = 0, right = 0;
 
-    if (!tree)
-        return (0);
-
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
-
-    return (1 + (left_height > right_height ? left_height : right_height));
+		if (tree->right)
+			right = 1 + binary_tree_height(tree->right);
+		if (tree->left)
+			left = 1 + binary_tree_height(tree->left);
+		if (left > right)
+			return (left);
+		else
+			return (right);
+	}
+	else
+		return (0);
 }
