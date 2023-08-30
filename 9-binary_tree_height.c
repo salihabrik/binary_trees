524f0fff89a80dee43b40eded129e74838a00459
salihabrik <briksaliha@gmail.com>
2023-08-30 14:18:44 +0200

    checks if a binary tree is perfect

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,20 +1,64 @@
 #include "binary_trees.h"
 
+
 /**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree to measure the height
+ * binary_tree_is_perfect - Checks if a binary tree is perfect.
+ * @tree: A pointer to the root node of the tree.
  *
- * Return: Height of the tree, 0 if tree is NULL
+ * Return: 1 if the tree is perfect, 0 otherwise.
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+    if (!tree)
+        return (0);
+
+    if (binary_tree_balance(tree) != 0)
+        return (0);
+
+    if (!tree->left && !tree->right)
+        return (1);
+
+    if (!tree->left || !tree->right)
+        return (0);
+
+    return (binary_tree_is_perfect(tree->left) &&
+            binary_tree_is_perfect(tree->right));
+}
+
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree.
+ * @tree: A pointer to the root node of the tree.
+ *
+ * Return: The balance factor.
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+    int left_height, right_height;
+
+    if (!tree)
+        return (0);
+
+    left_height = (int)binary_tree_height(tree->left);
+    right_height = (int)binary_tree_height(tree->right);
+
+    return (left_height - right_height);
+}
+
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: A pointer to the root node of the tree.
+ *
+ * Return: The height of the tree.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+    size_t left_height, right_height;
 
-	if (tree == NULL)
-		return (0);
+    if (!tree)
+        return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+    return (1 + (left_height > right_height ? left_height : right_height));
 }
