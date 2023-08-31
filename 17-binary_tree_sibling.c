1a7c908b20dafa3de4c2445a567b628c5efaedd5
salihabrik <briksaliha@gmail.com>
2023-08-31 15:28:53 +0200

    UPDATE

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,8 +10,8 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
-	if (node->parent->left == node)
-		return (node->parent->right);
-	else
+	if (node->parent->right == node)
 		return (node->parent->left);
+	else
+		return (node->parent->right);
 }
