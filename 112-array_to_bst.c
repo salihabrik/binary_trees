1b21c9de9f3d86e88c11f3440633a6ce541a9567
salihabrik <briksaliha@gmail.com>
2023-09-03 01:11:34 +0200

    check if a binary tree is a valid BST?

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -1,27 +1,23 @@
 #include "binary_trees.h"
 
 /**
- * array_to_bst - Builds a Binary Search Tree (BST) from an array.
+ * array_to_bst - Builds a Binary Search Tree from an array
  *
- * @array: Pointer to the first element of the array.
- * @size: Number of elements in the array.
+ * @array: Pointer to the first element of the array to be converted
+ * @size: The number of elements in the array
  *
- * Return: A pointer to the root node of the created BST, or NULL on failure.
+ * Return: Pointer to the root node of the created BST, or NULL on failure
  */
 bst_t *array_to_bst(int *array, size_t size)
 {
-    bst_t *root = NULL;
-    size_t i;
+	bst_t *root = NULL;
+	size_t i;
 
-    for (i = 0; i < size; i++)
-    {
-        if (bst_insert(&root, array[i]) == NULL)
-        {
-            /* Insertion failed, return NULL. Clean up any created nodes. */
-            binary_tree_delete(root);
-            return (NULL);
-        }
-    }
+	if (array == NULL || size == 0)
+		return (NULL);
 
-    return (root);
+	for (i = 0; i < size; i++)
+		bst_insert(&root, array[i]);
+
+	return (root);
 }
