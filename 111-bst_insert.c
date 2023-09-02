e83ed4772ef5b620d82e36a62c4224d8639fa88f
salihabrik <briksaliha@gmail.com>
2023-09-03 01:01:59 +0200

    check if a binary tree is a valid BST?

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,47 +1,59 @@
 #include "binary_trees.h"
 
 /**
- * bst_insert - Inserts a value in a Binary Search Tree (BST).
+ * bst_insert - Inserts a value into a Binary Search Tree
  *
  * @tree: A double pointer to the root node of the BST.
- * @value: The value to insert.
+ * @value: The value to insert into the BST.
  *
- * Return: A pointer to the created node or NULL on failure.
+ * Return: A pointer to the created node, or NULL on failure.
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-	if (*tree == NULL)
-	{
-		*tree = binary_tree_node(NULL, value);
-		if (*tree == NULL)
-			return (NULL);
-		return (*tree);
-	}
+    bst_t *new_node, *current;
 
-	if (value < (*tree)->n)
-	{
-		if ((*tree)->left == NULL)
-		{
-			(*tree)->left = binary_tree_node(*tree, value);
-			if ((*tree)->left == NULL)
-				return (NULL);
-			return ((*tree)->left);
-		}
-		else
-			return (bst_insert(&((*tree)->left), value));
-	}
-	else if (value > (*tree)->n)
-	{
-		if ((*tree)->right == NULL)
-		{
-			(*tree)->right = binary_tree_node(*tree, value);
-			if ((*tree)->right == NULL)
-				return (NULL);
-			return ((*tree)->right);
-		}
-		else
-			return (bst_insert(&((*tree)->right), value));
-	}
+    if (!tree)
+        return (NULL);
 
-	return (NULL);
+    if (*tree == NULL)
+    {
+        new_node = binary_tree_node(NULL, value);
+        if (!new_node)
+            return (NULL);
+        *tree = new_node;
+        return (new_node);
+    }
+
+    current = *tree;
+    while (current)
+    {
+        if (value == current->n)
+            return (NULL);
+
+        if (value < current->n)
+        {
+            if (!current->left)
+            {
+                new_node = binary_tree_node(current, value);
+                if (!new_node)
+                    return (NULL);
+                current->left = new_node;
+                return (new_node);
+            }
+            current = current->left;
+        }
+        else
+        {
+            if (!current->right)
+            {
+                new_node = binary_tree_node(current, value);
+                if (!new_node)
+                    return (NULL);
+                current->right = new_node;
+                return (new_node);
+            }
+        current = current->right;
+    }
 }
+
+return (NULL);
